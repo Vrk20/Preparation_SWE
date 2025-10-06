2059a9fa5f60fc2dd048973448b1a21b38c0a243
Vrk20 <kv192000@gmail.com>
2025-10-06 23:55:19 +0530

    Added sizeof info in arrays

--- DSA/Arrays/basics.cpp
+++ DSA/Arrays/basics.cpp
@@ -5,7 +5,8 @@
 */
 #include<iostream>
 
-void printarray(int* arr, int size){
+void printarray(int* arr, int size) // passing array as a pointer int* or int arr[] in this case size needs to explicitly passed
+{  
     for(int i=0;i<size;i++){
         std::cout<<arr[i]<<"\t";
     }
@@ -14,15 +15,20 @@ void printarray(int* arr, int size){
 int main(){
     int a[5] = {1,2,3,4,5}; // basic array implementation
     printarray(a,5);
+    int a2[3];  // array creation if no elements are assigned 
+    printarray(a2,3);  //Uninitialized arrays contain garbage values (whatever was in memory previously)
     int size_arr;
     std::cout<<"Enter the size of the array\n";  
     std::cin>>size_arr;
-    int arr[size_arr];
+    int arr[size_arr]; // NOTE: Variable Length Arrays (VLAs) are compiler extensions, not standard C++
+    std::cout<<"Enter "<<size_arr<<" elements:\n";
     for (int i = 0; i < size_arr; i++) // need to cover modern for techniques
     {
         std::cin>>arr[i];
     }
     printarray(arr,size_arr);
-    
+    // to know the memory allocated for an array in bytes
+    std::cout<<"size of one element of an array is\t"<<sizeof(arr[0])<<"\n";//size of an individual elements
+    std::cout<<"memory of total array is\t"<<sizeof(arr)<<"\n"; //size of an entire array
     return 0;
 }
