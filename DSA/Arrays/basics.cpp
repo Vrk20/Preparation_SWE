be868f31d6819deb23db0df5c588005b1830c2a5
Vrk20 <kv192000@gmail.com>
2025-11-17 23:39:06 +0530

    Function of vectors

--- DSA/Arrays/basics.cpp
+++ DSA/Arrays/basics.cpp
@@ -5,6 +5,25 @@
 */
 #include<iostream>
 
+// added grow function to have understanding of vector internal working
+int* grow(int* oldarr,int oldsize,int newsize){
+    int* newarr = new int[newsize];
+    for(int i=0;i<oldsize;i++){
+        newarr[i] = oldarr[i];
+    }
+    delete oldarr[];
+    return newarr;
+}
+
+// added sample push_back function from vector 
+void push_back(int*& arr,int& size,int& capacity,int x){
+    if(size == capacity){
+        arr = grow(arr,capacity,capacity*2);
+        capacity *=2;
+    }
+    arr[size] = x;
+    size++;
+}
 void printarray(int* arr, int size) // passing array as a pointer int* or int arr[] in this case size needs to explicitly passed
 {  
     for(int i=0;i<size;i++){
